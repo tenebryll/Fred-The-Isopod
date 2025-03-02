aa598e604b8c6d5eed748074c08c8969a8a022ac
Milo <79590178+tenebryll@users.noreply.github.com>
2025-03-02 22:23:45 +0000

    Update fred.py

--- fred.py
+++ fred.py
@@ -1,73 +1,82 @@
 import discord
 from discord.ext import commands
-
+token = "BOT-TOKEN"
+owner = "OWNER_USERNAME"
 intents = discord.Intents.default()
 intents.message_content = True
 intents.dm_messages = True
 intents.guilds = True
 fred = commands.Bot(command_prefix = '~', intents = intents)
-
+   
 @fred.event
 async def on_ready():
     print(f'{fred.user.name} is now Online!')
-#Owner Only commands
-@fred.command(name = "say", help = "Owner Only: Send a message to TextChannel as Fred.")
-async def say(ctx, destination: discord.TextChannel = None, *, message: str = None):
-    if ctx.author.id == ctx.guild.owner_id:
-        if destination:
-            if message:
-                try:
-                    await destination.send(message)
-                    await ctx.send(f"\"{message}\" sent to \"{destination.mention}\"")
-                except Exception as x:
-                    await ctx.send(f"Error: {x}")
-            else:
-                await ctx.send("Specify a Message")
-        else:
-            await ctx.send("Specify a TextChannel")
-    else:
-        await ctx.send("You are not my Master!")
 
-@fred.command(name = "dm", help = "Owner Only: Send dm without response as Fred.")
-async def dm(ctx, user: discord.Member = None, *, message: str = None):
-    if ctx.author.id == ctx.guild.owner_id:
-        if user:
-            if message:
-                try:
-                    await user.send(message)
-                    await ctx.send(f"\"{message}\" sent to \"{user.mention}\"")
-                except Exception as x:
-                    await ctx.send(f"Error: {x}")
-            else:
-                await ctx.send("No Message specified")
-        else:
-            await ctx.send("No User specified")
-    else:
-        await ctx.send("You are not my Master!")
-
-@fred.command(name = "msg", help = "Owner Only: Send dm with response as Fred.")
+# Command for Fred to DM people ( ~msg @person hello )
+@fred.command(name = "msg")
 async def msg(ctx, user: discord.Member = None, *, message: str = None):
-    if ctx.author.id == ctx.guild.owner_id:
-        if user:
-            if message:
-                try:
-                    await user.send(message)
-                    await ctx.send(f"\"{message}\" sent to \"{user.mention}\"")
-                    def check(message):
-                        return message.author == user and message.channel.type == discord.ChannelType.private
-                    try:
-                        response = await fred.wait_for('message', timeout = 180, check = check)
-                        await ctx.send(f"Response from {user.mention}: {response.content}")
-                        await user.send("Processing message..")
-                    except asyncio.TimeoutError:
-                        await ctx.send(f"No response from {user.mention}")
-                        await user.send("No response processed")
-                except Exception as x:
-                    await ctx.send(f"Error: {x}")
-            else:
-                await ctx.send("Specify a Message")
-        else:
-            await ctx.send("Specifiy a User")
+    if ctx.author.id != owner:
+        return await ctx.send("You are not permitted to run this command")
+    if not user:
+        return await ctx.send("Specifiy a User")
+    if not message:
+        await ctx.send("Specify a Message")
+    try:
+        await user.send(message)
+        await ctx.send(f"\"{message}\" sent to \"{user.mention}\"")
+        def check(message):
+            return message.author == user and message.channel.type == discord.ChannelType.private
+        try:
+            response = await fred.wait_for('message', timeout = 180, check = check)
+            await ctx.send(f"Response from {user.mention}: {response.content}")
+            await user.send("Awaiting your response..")
+        except asyncio.TimeoutError:
+            await ctx.send(f"No response from {user.mention}")
+            await user.send("No response processed")
+    except Exception as x:
+       await ctx.send(f"Error: {x}")
+
+# Command to have Fred talk in server (~say #general hello)
+@fred.command(name = "say")
+async def say(ctx, destination: discord.TextChannel = None, *, message: str = None):
+    if ctx.author.id != owner:
+        await ctx.send("You are not permitted to run this command")
+        return
+    if not destination:
+        await ctx.send("Specify a TextChannel")
+        return
+    if not message:
+        await ctx.send("Specify a Message")
+        return
+    try:
+        await destination.send(message)
+        await ctx.send(f"\"{message}\" sent to \"{destination.mention}\"")
+    except Exception as x:
+        await ctx.send(f"Error: {x}")
+
+@fred.command(name = "log")
+async def log(ctx):
+    if ctx.author.name.id == owner:
+        global log_channel
+        log_channel = ctx.channel.id
+        await ctx.send(f"Log channel set to {ctx.channel.mention}")
     else:
-        await ctx.send("You are not my Master!")
-fred.run('BotToken')
+        await ctx.send("You are not permitted to run this command")
+
+@fred.event
+async def on_message_delete(message):
+    if message.author.name != owner:
+        embed = discord.Embed(title="{} deleted the message:".format(message.author.name), description="", color=0xFF0000)
+        channel = fred.get_channel(log_channel)
+        await channel.send(channel, embed=embed)
+
+@fred.event
+async def on_message_edit(message_before, message_after):
+    if message_before.author.name != owner:
+        embed = discord.Embed(title="{} edited a message".format(message_before.author.name), description="", color=0xFF0000)
+        embed.add_field(name=message_before.content, value="Message before the edit", inline=True)
+        embed.add_field(name=message_after.content, value="Message after the edit", inline=True)
+        channel = fred.get_channel(log_channel)
+        await channel.send(channel, embed=embed)
+
+fred.run(token)
